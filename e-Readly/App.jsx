c67a82a6abf198b1583d21fb9300a9ec28700084
mamentudicky <dickymamentu@gmail.com>
2024-11-14 10:36:34 +0800

    perbaiki kesalahan di app.jsx

--- e-Readly/App.jsx
+++ e-Readly/App.jsx
@@ -1,10 +1,5 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './css/style.css'
-import './css/bootstrap.min.css'
-import './css/responsive.css'
-import './css/jquery.mCustomScrollbar.min.css'
-import './css/owl.carousel.min.css'
 
 import "./config/Firebase";
 
