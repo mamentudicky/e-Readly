6df3860cac43b4c61b3d2755f56e3bad8a4cc51b
mamentudicky <dickymamentu@gmail.com>
2024-11-14 10:35:12 +0800

    add header code

---
+++ e-Readly/App.jsx
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './css/style.css'
+import './css/bootstrap.min.css'
+import './css/responsive.css'
+import './css/jquery.mCustomScrollbar.min.css'
+import './css/owl.carousel.min.css'
+
+import "./config/Firebase";
+
+export default App
