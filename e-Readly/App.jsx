d5b8cc3e28f5d009083022ad2fc8a64dd5964cb0
GabsCool <jilmarcellino21@gmail.com>
2024-11-15 09:37:31 +0800

    configurasi dan html to react

--- e-Readly/App.jsx
+++ e-Readly/App.jsx
@@ -1,6 +1,16 @@
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./src/config/firebase";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Tambahkan route Anda di sini */}
+        <Route path="/" element={<Home />} />
+        {/* Contoh: <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
-import "./config/Firebase";
-
-export default App
+export default App;
